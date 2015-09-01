f878c80def88063491765d64a0b55a86d07a7aa6
44205349 <44205349@qq.com>
2015-09-01 17:36:29 +0800

    判断两本书编制是否相同

--- Sales.cpp
+++ Sales.cpp
@@ -2,8 +2,17 @@
 #include "Sales_item.h"
 int main(void)
 {
-	Sales_item book;
-	std::cin >> book;
-	std::cout << book << std::endl;
+	Sales_item book[2];
+	std::cin >> book[0];
+	std::cin >> book[1];
+	if(book[0].same_isbn(book[1]))
+	{
+		
+		std::cout << book[0] + book[1] << std::endl;
+	}
+	else{
+		std::cerr << "Date must refer to same ISBN" << std::endl;
+		return -1;
+	}
 	return 0;
 }
