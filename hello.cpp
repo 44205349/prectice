ffeb987ecca05877af7ea2e0a4adf43c06655bfa
44205349 <44205349@qq.com>
2015-09-01 16:33:21 +0800

    输出a+b的值

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(void)
+{
+	int a,b;
+	std::cout << "please input two number and I will output its multiply: " << std::endl;
+	std::cin >> a >> b;
+
+	std::cout << "Hello world!\nThe result is " <<  a*b << "!" << std::endl;
+	return 0;
+}
